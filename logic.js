69b21883fe0d62b89862fffa50e057425c3cdf3f
raphatapi <raphaeltapioca10@gmail.com>
2019-05-10 11:21:20 -0600

    added bootstrap

---
+++ logic.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    var typed = new Typed('#typed', {
+        strings: ["Web Designer", "Backend and Frontend", "Web Developer"],
+        typeSpeed: 100,
+        backSpeed: 30,
+        smartBackspace: true,
+        backDelay: 2000,
+        loop: true,
+        loopCount: Infinity
+    });
+});
