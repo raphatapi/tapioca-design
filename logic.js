4ab50fe8691494361e837349e88ddef023286e99
raphatapi <raphaeltapioca10@gmail.com>
2019-05-15 22:24:25 -0600

    navbar fade in-out

--- logic.js
+++ logic.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
+
+    // hide .navbar first
+    $(".navbar").hide();
+      
+    // fade in .navbar
+    $(function () {
+        $(".wrapper").on("scroll", function () {
+            // set distance user needs to scroll before we fadeIn navbar
+            if ($(this).scrollTop() > 200) {
+                $('.navbar').fadeIn();
+            } else {
+                $('.navbar').fadeOut();
+            }
+        });    
+    });
+
     var typed = new Typed('#typed', {
         strings: ["Web Designer", "Backend and Frontend", "Web Developer"],
         typeSpeed: 100,
