abf20ded8b6a9122eee2709f33039aeca236480d
raphatapi <raphaeltapioca10@gmail.com>
2019-05-21 23:34:12 -0600

    affix and cover section

--- logic.js
+++ logic.js
@@ -1,27 +1,15 @@
 $(document).ready(function() {
 
-    // hide .navbar first
-    $(".navbar").hide();
-      
-    // fade in .navbar
-    $(function () {
-        $(".wrapper").on("scroll", function () {
-            // set distance user needs to scroll before we fadeIn navbar
-            if ($(this).scrollTop() > 200) {
-                $('.navbar').slideDown("fast");
-            } else {
-                $('.navbar').slideUp("slow");
-            }
-        });    
-    });
-
     var typed = new Typed('#typed', {
         strings: ["Web Designer", "Backend and Frontend", "Web Developer"],
-        typeSpeed: 100,
+        typeSpeed: 70,
         backSpeed: 30,
         smartBackspace: true,
-        backDelay: 2000,
+        backDelay: 800,
         loop: true,
         loopCount: Infinity
     });
+
+
+
 });
